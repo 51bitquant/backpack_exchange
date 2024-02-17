c94c4edcd54fd10fc9e4719f3536b819ecbb306b
51bitquant <stephen0823@aliyun.com>
2024-02-17 21:54:59 +1030

    backpack

---
+++ config.py
@@ -0,0 +1,8 @@
+
+api_key = "你的api key"
+api_secret = "你的api secret"
+
+trade_quantity = 10  # 交易的下单数量.
+vol_precision = 2  # 数量精度， 保留两位小数.
+trading_fee = 0.001  # 千分之手续费.
+trade_pair = "SOL_USDC"  # 交易对.
